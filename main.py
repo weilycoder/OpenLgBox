b69be21c36fa26c7cc6d1471874f4b05aecc914e
weilycoder <weily2009@outlook.com>
2026-01-25 15:29:32 +0800

    feat: Add initials filtering option to command line arguments

--- main.py
+++ main.py
@@ -12,9 +12,11 @@ if __name__ == "__main__":
     parser.add_argument("-s", "--strict", action="store_true", help="Enable strict matching (all records must match)")
     parser.add_argument("-u", "--uid", type=int, default=None, help="Luogu API UID")
     parser.add_argument("-c", "--client_id", type=str, default=None, help="Luogu API Client ID")
+    parser.add_argument("-i", "--initials", type=str, default=None, help="Filter by guessed initials (use `,` to separate multiple)")
     parser.add_argument("--only-search", action="store_true", help="Only search for user info without filtering OIERDB")
 
     args = parser.parse_args()
+    initials = None if args.initials is None else args.initials.split(",")
     if args.uid is not None:
         if args.client_id is not None:
             print("Warning: Overriding config.uid with command line argument.")
@@ -46,6 +48,7 @@ if __name__ == "__main__":
         leave = list(
             filter(
                 lambda oier: oier.ccf_level >= ccfLevel
+                and (initials is None or oier.initials in initials)
                 and (not args.strict or len(oier.records) == len(prizes))
                 and all(any(record.check(*prize) for record in oier.records) for prize in prizes),
                 oierdb.oiers,
